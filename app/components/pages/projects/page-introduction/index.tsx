a647e57c9392b5b6501adfc34401838fbf0a34ca
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2024-09-01 20:11:50 -0300

    Estruturando a página de projetos

---
+++ app/components/pages/projects/page-introduction/index.tsx
@@ -0,0 +1,13 @@
+import { SectionTitle } from "@/app/components/section-title"
+
+export const PageIntroduction = () => {
+    return (
+        <section>
+            <SectionTitle 
+                subtitle="Projetos" 
+                title="Meus projetos" 
+                className="text-center items-center"
+            />
+        </section>
+    )
+}
