cad50656d86583fb79de73fe008b9b29a9c89ee9
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2024-09-10 02:23:13 -0300

    Gerando Rotas Estáticas durante o Build

--- app/components/pages/home/work-experience/index.tsx
+++ app/components/pages/home/work-experience/index.tsx
@@ -1,6 +1,6 @@
 import { SectionTitle } from '@/app/components/section-title'
 import { ExperienceItem } from './experience-item'
-import { WorkExperience as IWorkExperience } from '@/app/types/work-experience'
+import { WorkExperience as IWorkExperience } from "@/app/types/work-experience";
 
 type WorkExperienceProps = {
   experiences: IWorkExperience[]
